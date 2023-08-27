b19c462a812187f111ee069eba7dba6acf72f061
jhonnymalta <jhonatamalta@hotmail.com>
2023-08-26 23:31:29 -0300

    create product model

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Product,Category,Fabricant
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Fabricant)
+
+
--- apps/products/models.py
+++ apps/products/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from datetime import datetime
+
+
+class Fabricant(models.Model):
+    name = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+
+    class Meta:
+        ordering = ["country"]
+
+    def __str__(self) -> str:
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+    
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length= 100)
+    price = models.DecimalField(max_digits=5,decimal_places=2,blank=True)
+    qtd = models.IntegerField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    url = models.SlugField()
+    is_active = models.BooleanField()
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    fabricant = models.OneToOneField(Fabricant,on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+    
